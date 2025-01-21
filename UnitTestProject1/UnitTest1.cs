cd1443604440fc9548ac7778d0cc038dbe5e9c43
Yoann Floc'hlay <yoann.flochlay@sword-group.com>
2025-01-21 11:18:59 +0100

    Ajoutez des fichiers projet.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void Debit_WithValidAmount_UpdatesBalance()
+		{
+			// Arrange
+			double debitAmount = 4.55;
+			double expected = 7.44;
+
+
+			Assert.AreNotEqual(expected, debitAmount, "Account not debited correctly");
+		}
+
+		[TestMethod]
+		public void Debit_WithValidAmount_UpdatesBalance_ok()
+		{
+			// Arrange
+			double debitAmount = 7.44;
+			double expected = 7.44;
+
+
+			Assert.AreEqual(expected, debitAmount, "Account not debited correctly");
+		}
+	}
+}
